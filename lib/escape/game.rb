0c24f3d3b7aa3cd28040c509acc79fd35cce9c6f
Partho Ghosh <partho.ghosh24@gmail.com>
2017-03-01 00:26:04 +0530

    Added font for text. Working on text animation. Added binary to execute game

--- lib/escape/game.rb
+++ lib/escape/game.rb
@@ -1,17 +1,34 @@
 require 'gosu'
 module Escape
   class Game < Gosu::Window
+
+    FONT_ASSETS = {
+      font: "assets/fonts/pixelade-webfont.ttf"
+    }
+
     def initialize
       super 1280, 720, fullscreen: true
       self.caption = "Escape v0.1.0"
+      self.load_assets
     end
 
     def begin!
       self.show
     end
 
+    def draw
+
+      @font.draw("P", 20, 10, 3, 1, 1, Gosu::Color::WHITE)
+      @font.draw("a", 60, 10, 3, 1, 1, Gosu::Color::WHITE)
+      @font.draw("r", 100, 10, 3, 1, 1, Gosu::Color::WHITE)
+    end
+
     def button_up(key)
       self.close if key == Gosu::KbEscape
+    end
+
+    def load_assets
+      @font = Gosu::Font.new(self, FONT_ASSETS[:font],80)
     end
 
   end
